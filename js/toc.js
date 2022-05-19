488516101a26b06e9125b94132275d6e15d9b8bc
Lerina J-Y Razafy <lerina.razafy@gmail.com>
2022-05-19 19:31:07 -0400

    common js extracted

---
+++ js/toc.js
@@ -0,0 +1,11 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("TOC").style.width = "60%";
+  document.getElementsByTagName('main')[0].style.marginLeft = "65%";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("TOC").style.width = "0%";
+  document.getElementsByTagName('main')[0].style.margin = "0 auto";
+}
